80f37dbeca8dd9872e7ed42b94334e1f28765508
Chase Miller <46936509+ChaseMillers@users.noreply.github.com>
2022-07-08 10:29:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('Chases Dinner')
 
@@ -16,7 +16,7 @@ streamlit.header('Build Your Own Fruit Smoothie')
 streamlit.text("Moms Special Pancakes.")
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-my_fruit_list = my_fruit_list.set_index('Fruit')
+
 
                                         
 streamlit.dataframe(my_fruit_list)
