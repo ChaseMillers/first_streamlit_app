486c798ecef675f16d8b450d55be51a371b9d29a
Chase Miller <46936509+ChaseMillers@users.noreply.github.com>
2022-07-08 10:18:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.header('Build Your Own Fruit Smoothie')
 
 streamlit.text("Moms Special Pancakes.")
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', Strawberries])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
                                         
